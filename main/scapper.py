0c5516d56813587692cf0f840d8f58f209af499c
Moses <mosesamwoma@gmail.com>
2025-12-21 10:45:45 +0300

    deleted

--- main/scapper.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
-
-url="http://books.toscrape.com/?referrer=grok.com"
-page=requests.get(url)
-soup=BeautifulSoup(page.text,'html.parser')
-print(soup)
