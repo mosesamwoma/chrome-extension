bffcdecb5542c97de29b4cc3c53821077566ac54
Moses <mosesamwoma@gmail.com>
2025-12-31 16:27:26 +0300

    refactored the code and updateds

--- icons/background.js
+++ icons/background.js
@@ -1,8 +1,6 @@
-// background.js - Handle messages and updates
 chrome.runtime.onInstalled.addListener(() => {
     console.log('[Spam Shield] Extension installed');
     
-    // Initialize storage
     chrome.storage.local.set({
         scannedCount: 0,
         spamCount: 0,
@@ -14,7 +12,6 @@ chrome.runtime.onInstalled.addListener(() => {
     });
 });
 
-// Listen for messages from content script
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.type === 'UPDATE_STATS') {
         chrome.storage.local.get(['scannedCount', 'spamCount'], (data) => {
