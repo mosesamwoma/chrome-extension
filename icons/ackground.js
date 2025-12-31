185d68c5e5c2e9765304aae626387efc44ac8678
Moses <mosesamwoma@gmail.com>
2025-12-31 16:23:58 +0300

    updated

--- icons/ackground.js
+++ icons/ackground.js
@@ -1,10 +1,8 @@
-// This listens for when a user visits a new website
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === 'complete' && tab.url) {
     
-    // Example: Check if the URL contains "phishing" or "test-threat"
     if (tab.url.includes("suspicious-site.com")) {
-      // Change the icon to your 'warning' version
+
       chrome.action.setIcon({
         path: {
           "16": "images/icon16.png",
@@ -14,7 +12,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
         tabId: tabId
       });
       
-      // Add a red badge that says "!"
+      
       chrome.action.setBadgeBackgroundColor({color: '#FF0000'});
       chrome.action.setBadgeText({text: "!", tabId: tabId});
     }
